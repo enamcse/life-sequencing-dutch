9dd6600c68b9ae9b71eec9cbc95d16af8c3b6785
Enamul Hassan <enamsustcse@gmail.com>
2025-06-18 10:08:03 -0400

    Pipe test s3 preprocess param check 5

--- pipe_test_eh/s3_preprocess.sh
+++ pipe_test_eh/s3_preprocess.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
 #SBATCH --job-name=preprocess
-#SBATCH --output=/home/ehassan/test.out
-#SBATCH --error=/home/ehassan/logs/test.err
+#SBATCH --output=/home/ehassan/life-sequencing-dutch/logs/test.out
+#SBATCH --error=/home/ehassan/life-sequencing-dutch/logs/test.err
 #SBATCH --ntasks-per-node=1
 #SBATCH --nodes=1
 #SBATCH --time=0-12:00:00
