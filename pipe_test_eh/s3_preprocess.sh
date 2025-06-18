0d8b14e954d7ae796d7422c1eb1c82af99a672dc
Enamul Hassan <enamsustcse@gmail.com>
2025-06-18 10:19:20 -0400

    Pipe test s3 preprocess update typos in paths

--- pipe_test_eh/s3_preprocess.sh
+++ pipe_test_eh/s3_preprocess.sh
@@ -3,11 +3,9 @@
 #SBATCH --job-name=preprocess
 #SBATCH -e /home/ehassan/logs/%x.%j.err
 #SBATCH -o /home/ehassan/logs/%x.%j.out
-# #SBATCH --output=/home/ehassan/logs/%x.%j.out
-# #SBATCH --error=/home/ehassan/logs/%x.%j.err
 #SBATCH --ntasks-per-node=1
 #SBATCH --nodes=1
-#SBATCH --time=0-12:00:00
+#SBATCH --time=3:00:00
 # #SBATCH --partition=rome
 #SBATCH --mem=32G
 
@@ -15,7 +13,7 @@ echo "job started"
 
 date
 pwd
-source load_sbu_venv.sh
-python -m pipe_test_eh.preprocess_data pipe_test_eh/s3_preprocess_cfg.json
+source pipe_test_eh/load_sbu_venv.sh
+python -m pipe_test_eh.s3_preprocess_data pipe_test_eh/s3_preprocess_cfg.json
 
 echo "job ended successfully"
