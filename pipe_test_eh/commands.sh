379f68d424444c5c3b320918cd2d7f429c00f9c9
Enamul Hassan <enamsustcse@gmail.com>
2025-06-25 15:08:17 -0400

    Correct paths in commands.sh

--- pipe_test_eh/commands.sh
+++ pipe_test_eh/commands.sh
@@ -1,19 +1,19 @@
 # Generate Step 1 data: Run from pipe_test_eh directory
 # SBU AI cluster gave error due to capitalization of the file name fake_rinpersoon.csv but my local system did not.
 # mkdir step1
-python s1_generate_step1_data.py
+python pipe_test_eh/s1_generate_step1_data.py
 
 # Generate Step 2 data
-python s2_subset_data_by_columns.py step1 step2 s1_to_s2_column_list.txt
+python pipe_test_eh/s2_subset_data_by_columns.py step1 step2 s1_to_s2_column_list.txt
 
 # Generate Step 3 data
-sbatch s3_preprocess.sh
+sbatch pipe_test_eh/s3_preprocess.sh
 
 # Generate Step 4 data
-sbatch s4_create_parquet_seq.sh
+sbatch pipe_test_eh/s4_create_parquet_seq.sh
 
 # Generate Step 5 data
-sbatch s5_pipeline.sh
+sbatch pipe_test_eh/s5_pipeline.sh
 
 # Generate Step 6 data
-sbatch s6_pretrain_small.sh
+sbatch pipe_test_eh/s6_pretrain_small.sh
