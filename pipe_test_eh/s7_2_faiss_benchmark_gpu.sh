9a7226f6d10791d71d2b1e2a4c52003a94a8f5d6
Enamul Hassan <enamsustcse@gmail.com>
2025-06-26 13:28:51 -0400

    Fix sh in faiss to request gpu

--- pipe_test_eh/s7_2_faiss_benchmark_gpu.sh
+++ pipe_test_eh/s7_2_faiss_benchmark_gpu.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=faiss_benchmark
-#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
 #SBATCH --nodes=1
 #SBATCH --time=01:00:00
-#SBATCH --mem=32G
+#SBATCH --mem=80G
+#SBATCH --gpus-per-node=1
 #SBATCH -e /home/ehassan/logs/%x.%j.err
 #SBATCH -o /home/ehassan/logs/%x.%j.out
 
@@ -12,3 +13,4 @@ source pipe_test_eh/load_sbu_venv.sh
 echo "Starting FAISS Benchmark..."
 time python -m pipe_test_eh.s7_2_faiss_benchmark_gpu --config=pipe_test_eh/s7_2_faiss_benchmark_gpu_cfg.json
 echo "FAISS Benchmark completed."
+
